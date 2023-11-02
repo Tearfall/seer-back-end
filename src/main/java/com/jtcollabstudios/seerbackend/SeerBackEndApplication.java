e361b5187f370c446e4228bb6ca960cc4340b253
Jay Tinosa <jaytinosaprogramming@gmail.com>
2023-11-02 13:23:28 +0800

    Initial commit

---
+++ src/main/java/com/jtcollabstudios/seerbackend/SeerBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.jtcollabstudios.seerbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeerBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeerBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jtcollabstudios/seerbackend/SeerBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtcollabstudios.seerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeerBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
